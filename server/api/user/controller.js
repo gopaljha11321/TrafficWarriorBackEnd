0a281f7a2a638cf5e38ccdc0e44217db455c2bbd
Gopal Jha <82890881+gopaljha11321@users.noreply.github.com>
2023-01-29 11:51:35 +0530

    image add in git

--- server/api/user/controller.js
+++ server/api/user/controller.js
@@ -1,6 +1,9 @@
 const mongo  = require('mongodb').MongoClient;
+const fs = require('file-system');
+
 url="mongodb+srv://gopal:jhaji9871436400@cluster0.it4owmc.mongodb.net/?retryWrites=true&w=majority"
 const dbName = 'project_ai';
+const path=require('path')
 const client= new mongo(url,(err)=>
 {
     if(err) throw err;
@@ -8,6 +11,7 @@ const client= new mongo(url,(err)=>
 client.connect()
 const db = client.db(dbName);
 const collection =db.collection("user")
+let image_name=""
 exports.detail=(req,res)=>
 {
     let id=req.body.id;
@@ -27,4 +31,36 @@ exports.detail=(req,res)=>
         res.send(error);
       }
     }())
+}
+exports.profile=(req,res)=>
+{
+  id=req.body.id
+  var img = fs.readFileSync(req.file.path);
+    var encode_image = img.toString('base64');
+    var finalImg = {
+      contentType: req.file.mimetype,
+      image: Buffer.from(encode_image, 'base64')
+  };
+  collection.updateOne({"_id":id},{$set: {"profile":finalImg}},(err)=>
+  {
+    if(err)
+    {
+      return res.send(err);
+    }
+    return res.send("file uploaded")
+  })
+}
+exports.profile_get=async(req,res)=>
+{
+  let user=req.query["user"];
+  let data=await collection.find({"_id":user}).toArray();
+  if(data.length>0)
+  {
+  const result=data[0].profile;
+  res.contentType(result.contentType);
+  res.send(result.image.buffer)
+  }
+  else{
+    res.send("error occure");
+  }
 }
--- server/api/user/route.js
+++ server/api/user/route.js
@@ -1,7 +1,17 @@
-const user =require("./controller")
+const user = require("./controller");
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./server/static/profile_image");
+  },
+  filename: (req, file, cb) => {
+    cb(null, path.basename(file.originalname));
+  },
+});
+const upload = multer({ storage: storage });
 module.exports = function (router) {
-	router.post(
-		'/user',
-        user.detail
-	);
+  router.post("/user", user.detail),
+    router.post("/upload_profile_image", upload.single("image"), user.profile),
+    router.get("/profile_image", user.profile_get);
 };
