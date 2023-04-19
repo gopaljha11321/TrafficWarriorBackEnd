0a14b70adf52265b86a2b7f7a006baddfba160d3
Gopal Jha <gopal.jha@UE-M-NOTE-0263.local>
2023-04-20 00:29:23 +0530

    create question api and average api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const mongo = require("mongodb").MongoClient;
-url ="mongodb+srv://gopal:jhaji9871436400@cluster0.it4owmc.mongodb.net/?retryWrites=true&w=majority";
+url =
+  "mongodb+srv://gopal:jhaji9871436400@cluster0.it4owmc.mongodb.net/?retryWrites=true&w=majority";
 const dbName = "project_ai";
 var bodyParser = require("body-parser");
 const client = new mongo(url, (err) => {
@@ -9,35 +10,32 @@ const client = new mongo(url, (err) => {
 client.connect();
 const db = client.db(dbName);
 const collection = db.collection("user");
-const core=require("cors");
+const core = require("cors");
 const router = express.Router();
 var app = express();
-const env=require("./server/config/env")
+const env = require("./server/config/env");
 var port = process.env.PORT || 3001;
-app.use(core({
-  origin:["http://127.0.0.1:3000","https://glittery-dango-a782d9.netlify.app"],
-}))
+app.use(
+  core({
+    origin: "*",
+  })
+);
 app.use(bodyParser.json());
-app.use(async(req,res,next)=>
-{
-  const notSecureUrl=["/login","/register","/profile_image","/testing"]
-  const original=req._parsedUrl.pathname;
-  const auth=req.headers.key;
-  let result = auth?await collection.find({ auth: auth }).toArray():[];
-  if(notSecureUrl.includes(original)==true)
-  {
+app.use(async (req, res, next) => {
+  const notSecureUrl = ["/login", "/register", "/profile_image", "/testing"];
+  const original = req._parsedUrl.pathname;
+  const auth = req.headers.key;
+  let result = auth ? await collection.find({ auth: auth }).toArray() : [];
+  if (notSecureUrl.includes(original) == true) {
     next();
-  }
-  else if(result.length>0)
-  {
+  } else if (result.length > 0) {
     next();
+  } else {
+    res.send("Sorry, you don't have a license");
   }
-  else{
-    res.send("Sorry, you don't have a license")
-  }
-})
+});
 require(`${__dirname}/server/app`)(app, router);
 
 app.listen(port, () => {
-  console.log("server Start");
+  console.log("server Start at :", port);
 });
--- server/APIs/evaluate/route.js
+++ server/APIs/evaluate/route.js
@@ -2,4 +2,6 @@ const controller = require("./controller");
 module.exports = function (router) {
   router.post("/evaluate", controller.evaluate),
     router.post("/addQuestion", controller.sample);
+  router.get("/questions", controller.allQuestion);
+  router.get("/average", controller.average);
 };
